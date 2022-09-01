9e62ff87c4a8ff3e7b27b73b2e775ee1c454f38c
Matteo Pietro Dazzi <ilteoood@MDazzi-ITMAC20.local>
2022-09-01 23:16:35 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { render } from 'preact'
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
+import { App } from './app'
+import './index.css'
+
+const retrieveContainer = props => (props.container ?? document).querySelector('#app')
+
+const renderApp = (props = {}) => {
+    render(<App />, retrieveContainer(props))
+}
+
+renderWithQiankun({
+    mount(props) {
+        renderApp(props)
+    },
+    bootstrap() { },
+    unmount() {
+        render(null, retrieveContainer(props))
+    },
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    renderApp({})
+}
